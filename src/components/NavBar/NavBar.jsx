09f4400ca88a641f18408ff40b11d8b92b7b7ec4
Aarón López <n_aaron5@hotmail.com>
2022-02-26 14:11:01 +0100

    navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,35 +1,40 @@
 import React from "react";
-import { Container, Nav, NavDropdown, Navbar} from "react-bootstrap";
-import logoUser from "../../assets/images/user.png"
-import logoCoin from "../../assets/images/coin.jpeg"
+import { Container, Nav, NavDropdown, Navbar, Row, Col } from "react-bootstrap";
+import logoUser from "../../assets/images/user.png";
+import logoCoin from "../../assets/images/coin.png";
+import "./NavBar.css";
 
 const NavBar = () => {
-
   return (
     <Navbar bg="primary" expand="lg">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <img
-        src={logoCoin}
-        width="60"
-        height="30"
-        className="d-inline-block align-top ml-5"
-        alt="coin icon"
-      />
-      <p>150</p>
-        <img
-        src={logoUser}
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="user icon"
-      />
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-          <Nav className="me-auto">
+      <Container className="">
+        <Row xs="" className="">
+          <Col className="d-flex align-items-center">
+            <img
+              src={logoUser}
+              width="40"
+              height="40"
+              className="m-2"
+              alt="user icon"
+            />
+            <img
+              id="coin-img"
+              src={logoCoin}
+              width="20"
+              height="20"
+              className="m-2"
+              alt="coin icon"
+            />
+            <p className="mt-3">150</p>
+          </Col>
+          <Col className="container-menu d-flex flex-column">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Navbar.Brand href="/">Aso Game</Navbar.Brand>
+              <Nav className="me-auto">
                 <Nav.Link href="">Sign Up</Nav.Link>
                 <Nav.Link href="">Log In</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -40,8 +45,10 @@ const NavBar = () => {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown> */}
-          </Nav>
-        </Navbar.Collapse>
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
